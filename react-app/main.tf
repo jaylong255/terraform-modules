98303de1314daec6456a439bb2afcf3d103c7228
Jay Long <jaylong255@gmail.com>
2022-08-23 10:32:01 -0500

    adding bucket acl

--- react-app/main.tf
+++ react-app/main.tf
@@ -10,5 +10,9 @@ terraform {
 # Create an S3 bucket with a cloudfront distribution
 resource "aws_s3_bucket" "bucket" {
   bucket = "${var.stack_name}-${var.app}-${var.region}-builds"
+}
+
+resource "aws_s3_bucket_acl" "bucket" {
+  bucket = "${aws_s3_bucket.bucket.bucket}"
   acl    = "public-read"
 }
