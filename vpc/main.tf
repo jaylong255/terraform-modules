1d396258a59d84f93bec94cba14eff6fa67b9157
Jay Long <jaylong255@gmail.com>
2022-08-24 18:45:31 -0500

    adding private web subnets to vpc

--- vpc/main.tf
+++ vpc/main.tf
@@ -9,13 +9,43 @@ terraform {
 
 # vpc
 resource "aws_vpc" "vpc" {
-  cidr_block = "var.cidr_block"
+  cidr_block = var.cidr_block
   enable_dns_hostnames = true
-
     tags = {
         Name = "${var.stack_name}-${var.env}-vpc"
         Project = var.stack_name
         Environment = var.env
         Terraform = "true"
     }
+}
+
+# subnets
+resource "aws_subnet" "web_subnet_a" {
+  cidr_block = var.cidr_block_web_a
+  vpc_id = aws_vpc.vpc.id
+  availability_zone = var.availability_zone_a
+  map_public_ip_on_launch = false
+  tags = {
+      Name = "${var.stack_name}-${var.env}-web-subnet-a"
+      Project = var.stack_name
+      Environment = var.env
+      Terraform = "true"
+      Type = "private"
+      Web = "true"
+  }
+}
+
+resource "aws_subnet" "web_subnet_b" {
+  cidr_block = var.cidr_block_web_b
+  vpc_id = aws_vpc.vpc.id
+  availability_zone = var.availability_zone_b
+  map_public_ip_on_launch = false
+  tags = {
+      Name = "${var.stack_name}-${var.env}-web-subnet-b"
+      Project = var.stack_name
+      Environment = var.env
+      Terraform = "true"
+      Type = "private"
+      Web = "true"
+  }
 }
--- vpc/vars.tf
+++ vpc/vars.tf
@@ -13,7 +13,32 @@ variable "env" {
   description = "Environment"
 }
 
+variable "subnet_count" {
+  default = "2"
+  description = "Number of subnets"
+}
+
 variable "cidr_block" {
   default = "10.0.0.0/16"
   description = "CIDR block for VPC"
+}
+
+variable "availability_zone_a" {
+  default = "us-east-1a"
+  description = "Availability zone A"
+}
+
+variable "availability_zone_b" {
+  default = "us-east-1b"
+  description = "Availability zone B"
+}
+
+variable "cidr_block_web_a" {
+  default = "10.0.0.0/22"
+  description = "CIDR block for web tier A"
+}
+
+variable "cidr_block_web_b" {
+  default = "10.0.4.0/22"
+  description = "CIDR block for web tier B"
 }
